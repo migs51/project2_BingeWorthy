dcaf840ab5155b58aae7d869990163f9039500ba
Migs <mmejares@hotmail.com>
2020-04-29 11:42:22 -0500

    skfj

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  var signUpForm = $("#formclass");
+  var emailInput = $("#inline-full-name");
+  var passwordInput = $("#inline-username");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
@@ -11,16 +11,20 @@ $(document).ready(function() {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
+    
+    
 
     if (!userData.email || !userData.password) {
       return;
     }
+
+    
     // If we have an email and password, run the signUpUser function
     signUpUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
-
+  
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function signUpUser(email, password) {
