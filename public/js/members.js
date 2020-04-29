0899aec326a7455dfa343fa14a98df78149b59f4
Migs <mmejares@hotmail.com>
2020-04-28 23:56:38 -0500

    added ajax post calls to get user recommendation to DB

--- public/js/members.js
+++ public/js/members.js
@@ -1,25 +1,48 @@
 $(document).ready(function() {
   //global variables
-  var thumbsUp = $("#thumbsUp");
-  var thumbsDown = $("#thumbsDown");
+  const thumbsUp = $("#thumbsUp");
+  const thumbsDown = $("#thumbsDown");
+  const showName = $("#showName");
+  const userName = $("#userEmail");
+  
 
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  $.get("/api/user_data").then(function(data) {
+  var userEmail = $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
 
-  function upVote() {
-    thumbsUp.click(function() {
-      console.log("it worked");
+  console.log(userEmail);
+  console.log(userName.val());
+
+  let userRecco = {
+    users: "Joe",
+    shows:  "West World",
+    bingeable: true
+  }
+
+  console.log(userRecco);
+
+
+
+  function upVote(post) {
+    $.post("/api/shows/recommendations", post, function() {
+      window.location.href = "/members";
     });
   }
+
+  thumbsUp.click(function() {
+    upVote(userRecco);
+    console.log("button clicked");
+  });
 
   function downVote() {
     thumbsDown.click(function() {
       console.log("It worked");
+
+
     });
   }
-  upVote();
+  
   downVote();
 });
